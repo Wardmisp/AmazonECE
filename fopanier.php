8c6f2efc8683349567dc0ae0ca76179a6780d45f
victortroussard <victor.troussard@hotmail.fr>
2019-04-30 16:54:12 +0200

    dernière version

---
+++ fopanier.php
@@ -0,0 +1,160 @@
+
+<?php
+session_start();
+include_once("fonctions-panier2.php");
+
+$erreur = false;
+
+$action = (isset($_POST['action'])? $_POST['action']:  (isset($_GET['action'])? $_GET['action']:null )) ;
+if($action !== null)
+{
+   if(!in_array($action,array('ajout')))
+   $erreur=true;
+
+   //récuperation des variables en POST ou GET
+   $l = (isset($_POST['l'])? $_POST['l']:  (isset($_GET['l'])? $_GET['l']:null )) ;
+   $p = (isset($_POST['p'])? $_POST['p']:  (isset($_GET['p'])? $_GET['p']:null )) ;
+   $q = (isset($_POST['q'])? $_POST['q']:  (isset($_GET['q'])? $_GET['q']:null )) ;
+
+   //Suppression des espaces verticaux
+   $l = preg_replace('#\v#', '',$l);
+   //On verifie que $p soit un float
+   $p = floatval($p);
+
+   //On traite $q qui peut etre un entier simple ou un tableau d'entier
+    
+   if (is_array($q)){
+      $QteArticle = array();
+      $i=0;
+      foreach ($q as $contenu){
+         $QteArticle[$i++] = intval($contenu);
+      }
+   }
+   else
+   $q = intval($q);
+    
+}
+
+if (!$erreur){
+   switch($action){
+      Case "ajout":
+         ajouterArticle($l,$q,$p);
+         break;
+
+
+      Default:
+         break;
+   }
+}
+
+echo '<?xml version="1.0" encoding="utf-8"?>';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+<title>Votre panier</title>
+</head>
+<body>
+
+<form method="post" action="panier.php">
+<table style="width: 400px">
+	<tr>
+		<td colspan="4">Votre panier</td>
+	</tr>
+	<tr>
+		<td>Libellé</td>
+		<td>Quantité</td>
+		<td>Prix Unitaire</td>
+		
+	</tr>
+
+
+	<?php
+	if (creationPanier())
+	{
+	   $nbArticles=count($_SESSION['panier']['libelleProduit']);
+	   if ($nbArticles <= 0)
+	   echo "<tr><td>Votre panier est vide </ td></tr>";
+	   else
+	   {
+	      for ($i=0 ;$i < $nbArticles ; $i++)
+	      {
+	         echo "<tr>";
+	         echo "<td>".htmlspecialchars($_SESSION['panier']['libelleProduit'][$i])."</ td>";
+	         echo "<td><input type=\"text\" size=\"4\" name=\"q[]\" value=\"".htmlspecialchars($_SESSION['panier']['qteProduit'][$i])."\"/></td>";
+	         echo "<td>".htmlspecialchars($_SESSION['panier']['prixProduit'][$i])."</td>";
+	         echo "<td><a href=\"".htmlspecialchars("panier.php?action=suppression&l=".rawurlencode($_SESSION['panier']['libelleProduit'][$i]))."\">XX</a></td>";
+	         echo "</tr>";
+	      }
+
+	      echo "<tr><td colspan=\"2\"> </td>";
+	      echo "<td colspan=\"2\">";
+	      echo "Total : ".MontantGlobal();
+	      echo "</td></tr>";
+
+	      echo "<tr><td colspan=\"4\">";
+	      echo "<input type=\"submit\" value=\"Rafraichir\"/>";
+	      echo "<input type=\"hidden\" name=\"action\" value=\"refresh\"/>";
+
+	      echo "</td></tr>";
+	   }
+	}
+	?>
+</table>
+</form>
+</body>
+</html>
+<!doctype.html>
+<html>
+<head>
+<meta charset=utf-8 />
+</head>
+<body>
+<script type="text javascript">
+<button type="button" class="add-to-cart" data-id="1">Ajouter au panier</button><!-- bouton ajouter au panier pas fini -->
+</script>
+<table class="table"><!-- page panier -->
+  <thead>
+    <tr><th>photo</th><th>description<th>prix</th><th>quantité</th></tr>
+  </thead>
+  <tbody id="cart-tablebody">
+  </tbody>
+</table>
+
+<p>Sous total : <span class="subtotal"></span>€</p>
+
+<button id="confirm-command">Passer à la commande</button>
+</body>
+</html>
+<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=amazonece;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM items');
+
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+    <?php echo $donnees['IDI']; ?><br />
+    <?php echo $donnees['nomI']; ?>, <?php echo $donnees['nomI']; ?><br />
+     <?php echo $donnees['descriptionI']; ?><?php echo $donnees['categorie']; ?> <br />
+    
+   </p>
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
